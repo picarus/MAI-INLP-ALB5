b15a9c1967240d307cf4ff6bc1344d8d5305d583
Jose Antonio Magaña Mesa <jose_a_magana@hotmail.com>
2013-10-04 14:53:03 +0200

    Structure of the project. clean() module to obtain text from html

---
+++ src/main.py
@@ -0,0 +1,24 @@
+'''
+Created on Oct 4, 2013
+
+@author: Jose
+'''
+
+from bs4 import BeautifulSoup as BS
+from url2text import clean
+
+if __name__ == '__main__':
+    
+    ### iterate the src
+    
+    text = clean(); # return text without html
+    
+    # preprocessing
+    # tokenize
+    # stopwords
+    # stemming
+    # named entities
+    
+    # tagging
+    
+    pass
--- src/url2text.py
+++ src/url2text.py
@@ -1,30 +1,34 @@
 import urllib2
 from bs4 import BeautifulSoup
 
-inFile = open('Dancers_urls.txt', 'r')
-
-user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-headers = {'User-Agent':user_agent,}
-text = ''
-
-#url = inFile.readline()
-url = 'http://www.technologyreview.com/view/419223/using-neural-networks-to-classify-music/'
-request = urllib2.Request(url,None,headers) #The assembled request
-response = urllib2.urlopen(request)
-data = response.read() # The data u need
-response.close()
+###inFile = open('Dancers_urls.txt', 'r')
 
 
-# HTML A TEXT
-soup = BeautifulSoup(data)
-text_parts = soup.find_all('p')
-i=0
-string_list = []
-for item in text_parts:
-    i=i+1
-    for string in item.stripped_strings:
-        string_list.append(repr(string))
+def getFile(url):
+    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+    headers = {'User-Agent':user_agent,}
+    
+    request = urllib2.Request(url,None,headers) #The assembled request
+    response = urllib2.urlopen(request)
+    data = response.read() # The data u need
+    response.close()
+    return data;
 
-text = ''.join(string_list).replace("'",'')
-
-print(text)
+def clean():
+    url = 'http://en.wikipedia.org/wiki/Mick_Jagger'
+    data=getFile(url);
+    
+    # HTML A TEXT
+    soup = BeautifulSoup(data)
+    text_parts = soup.find_all('p')
+    i=0
+    string_list = []
+    for item in text_parts:
+        i=i+1
+        for string in item.stripped_strings:
+            string_list.append(repr(string))
+    
+    text = ''.join(string_list).replace("'",'')
+    
+    print(text)
+    return text
