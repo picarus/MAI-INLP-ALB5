e6b3c3cf25cbfb63d773fd414499c8c177e25328
Jose Antonio Magaña Mesa <jose_a_magana@hotmail.com>
2013-11-09 09:34:39 +0100

    added comments to the code

--- src/namedEntity.py
+++ src/namedEntity.py
@@ -1,13 +1,15 @@
 
 '''
 Created on Oct 10, 2013
-
 @author: Jose
 '''
 
 import nltk
 
 def initText():
+    """ Initializes a simple text to be used for testing the processing part of the module.
+    A list of sentences with different Name Entity types, reqular and irregular verbs and some unicode characters are included.
+    """
 
     tt=u''' Peter lived in San Francisco.
     Michael enjoyed the travel to Almer\u00eda.
@@ -21,6 +23,9 @@ def initText():
     return tt
 
 def processNE(sentences):
+    """ Takes a list of sentences stored in an NLTK Tree structure and does Name Entity Recognition generating intermediate nodes
+    with the Named Entity discovered. The sentences must be tokenized and tagged prior to the NE recognition process.
+    """
     
     sentences = [ nltk.ne_chunk(sent) for sent in sentences]
     
